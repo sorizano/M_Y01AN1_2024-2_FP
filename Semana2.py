96c10b0bc0e2af1ca3bcf12c4bcc770f53fc3f4e
Sergio Orizano Salvador <svorizano@gmail.com>
2024-09-19 22:14:56 -0500

    Agregando Descripción

--- Semana2.py
+++ Semana2.py
@@ -2,3 +2,6 @@ import streamlit as st
 
 #titulo a la aplicación
 st.title("Introducción a Variables y tipos de datos en Python")
+
+#Descripción Incial
+st.write("Python es un lenguaje de programación dinámico donde ...")
