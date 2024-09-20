5d88d7347700b3832404f676e8761e598f63051f
Sergio Orizano Salvador <svorizano@gmail.com>
2024-09-19 22:23:00 -0500

    defiendo valores entreros

--- Semana2.py
+++ Semana2.py
@@ -5,3 +5,11 @@ st.title("Introducción a Variables y tipos de datos en Python")
 
 #Descripción Incial
 st.write("Python es un lenguaje de programación dinámico donde ...")
+
+#Sección para variable de tipo entero
+st.header("Ejemplo 1: Enteros")
+st.write("En pyton, un entero (integer) es un número sin decimales por ejemplo:")
+
+#Definir una variable entera
+int_variable = 42
+st.code(f"int_variable = {int_variable} # Tipo: {type(int_variable)}")
