4b4bbdc88009352e44afdc173a196ca6bfb03d03
Sergio Orizano Salvador <svorizano@gmail.com>
2024-10-17 21:45:14 -0500

    segundo ejemplo 5 con botón diseñado

--- Semana6.py
+++ Semana6.py
@@ -7,3 +7,23 @@ st.subheader("Ejercicio 5: Números pares entre 0 y 100")
 pares_0_100 = [i for i in range(0,101) if i % 2 == 0]
 st.write("Números pares entre 0 y 100:")
 st.write(pares_0_100)
+
+
+#Ejercicio5 con botón
+st.makdown("""
+    <style>
+    div.stButton > button{
+        background-color: #8B0000;
+        color: white;
+        font-size: 16px;
+        padding; 10px;
+        border-radius: 10px;
+        border: none;
+    }
+    </style>
+""", unsafe_allow_html=true)
+
+if st.button("Mostrar números pares entre 0 y 100"):
+    pares0a100 = [i for i in range(0,101) if i % 2 == 0]
+    st.write("Número pares entre 0 y 100:")
+    st.write(pares0a100)
