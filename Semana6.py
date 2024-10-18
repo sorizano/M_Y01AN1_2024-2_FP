9fd8ac5ef5e04cfb63a07672d9f5df6bdfd5b853
Sergio Orizano Salvador <svorizano@gmail.com>
2024-10-17 22:18:13 -0500

    ejercicio6 tema12

--- Semana6.py
+++ Semana6.py
@@ -27,3 +27,33 @@ if st.button("Mostrar números pares entre 0 y 100"):
     pares0a100 = [i for i in range(0,101) if i % 2 == 0]
     st.write("Número pares entre 0 y 100:")
     st.write(pares0a100)
+
+st.write("############################################################################")
+
+#Función para generar la serie numérica
+def generar_serie(n):
+    serie = []
+    elementos = [1, 5, 3, 7, 5, 9, 7] 
+    for i in range(n):
+        serie.append(elementos[i % len(elementos)])
+    return serie
+
+#Función para validar y calcular la suma
+def calcular_suma_serie(n):
+    if n <= 0:
+        return "El valor de n debe de ser mayor que 0"
+    serie = generar_serie(n)
+    suma = sum(serie)
+    return suma, serie
+
+#Streamlit para obtener el valor de n
+st.title("Ejercicio 6: Suma de los primeros n elementos de la serie")
+
+#Ingresar un número por teclado
+n = st.number_input("ingresa el valor de n:", min_value=1, step=1)
+
+#Botón para calcular la suma
+if st.button("Calcular suma"):
+    suma,serie = calcular_suma_serie(n)
+    st.write(f"La serie generada para n={n} es: {serie}")
+    st.write(f"La suma de los primeros {n} elementos es: {suma}")
