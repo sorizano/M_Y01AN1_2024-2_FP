f225bf8500485ccbc6127ce74e6b6b9068853aec
Sergio Orizano Salvador <svorizano@gmail.com>
2024-10-24 21:20:11 -0500

    creando arhchivo semana 7

---
+++ Semana7.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+def verificar_contrasena():
+    st.title("Verificación de contraseña")
+
+    #Variable para almacenar la contraseña
+    contrasena_correcta = "asdasd"
+    contrasena_ingresada = ""
+
+    #Repetir hasta que la contraseña sea correcta
+    while contrasena_ingresada != contrasena_correcta:
+        contrasena_ingresada = st.text_input("Ingrese la contraseña", type="password")
+        if contrasena_ingresada == contrasena_correcta:
+            st.success("Bienvenido")
+            break
+        elif contrasena_ingresada:
+            st.error("Contraseña incorrecta, intente de nuevo")
+if __name__ == "__main__":
+    verificar_contrasena()
