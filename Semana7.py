379ade634f170582740bd6f41f001b84fad01f6f
Sergio Orizano Salvador <svorizano@gmail.com>
2024-10-24 21:23:20 -0500

    Nuevo cambio

--- Semana7.py
+++ Semana7.py
@@ -3,18 +3,18 @@ import streamlit as st
 def verificar_contrasena():
     st.title("Verificación de contraseña")
 
-    #Variable para almacenar la contraseña
+    # Variable para almacenar la contraseña
     contrasena_correcta = "asdasd"
     contrasena_ingresada = ""
 
-    #Repetir hasta que la contraseña sea correcta
+    # Repetir hasta que la contraseña sea correcta
     while contrasena_ingresada != contrasena_correcta:
-        contrasena_ingresada = st.text_input("Ingrese la contraseña", type="password")
+        contrasena_ingresada = st.text_input("Introduce la contraseña:", type="password")
         if contrasena_ingresada == contrasena_correcta:
             st.success("Bienvenido")
             break
         elif contrasena_ingresada:
-            st.error("Contraseña incorrecta, intente de nuevo")
+            st.error("Contraseña incorrecta, intenta de nuevo")
 
 if __name__ == "__main__":
     verificar_contrasena()
